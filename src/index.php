8bd09dc39a6592380fce8e1605c319f94bf7a9ca
Yaroslav Selyutin <moontesearch@gmail.com>
2025-07-24 23:41:11 +1000

    feat: add header, add hero, add main section

---
+++ src/index.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html lang="ru">
+<?php include './assets/components/head.html'; ?>
+<body>
+    <?php include './assets/components/header.html'; ?>
+    <?php include './assets/components/main.html'; ?>
+    <?php include './assets/components/footer.html'; ?>
+</body>
+</html>
